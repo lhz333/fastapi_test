40886161659a3d904fa13af815418f5ae22a24b1
Jenny <18839970925@163.com>
2026-08-14 20:16:05 +0800

    login

--- news_backend/crud/users.py
+++ news_backend/crud/users.py
@@ -1,11 +1,12 @@
 import uuid
 from datetime import datetime, timedelta
 
-from sqlalchemy import select
+from fastapi import HTTPException
+from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from models.users import User, UserToken
-from schemas.users import UserRequest
+from schemas.users import UserRequest, UserUpdateRequest
 from utils import security
 
 # 通过用户名查询用户信息
@@ -24,6 +25,7 @@ async def create_user(db: AsyncSession, user_data: UserRequest):
     await db.refresh(user)
     return user
 
+# 生成token
 async def create_token(db: AsyncSession, user_id: int):
     token = str(uuid.uuid4())
     expires_at = datetime.now() + timedelta(days=7)
@@ -40,3 +42,54 @@ async def create_token(db: AsyncSession, user_id: int):
         await db.commit()
 
     return token
+
+# 登录验证用户密码
+async def verify_user(db: AsyncSession, user_data: UserRequest):
+    user = await get_user_by_username(db, user_data.username)
+    if not user:
+        return None
+    if not security.verify_password(user_data.password, user.password):
+        return None
+    return user
+
+# 根据token查询用户信息
+async def get_user_by_token(db: AsyncSession, token: str):
+    stmt = select(UserToken).where(UserToken.token == token)
+    result = await db.execute(stmt)
+    db_token = result.scalar_one_or_none()
+
+    if not db_token or db_token.expires_at < datetime.now():
+        return None
+
+    query = select(User).where(db_token.user_id == User.id)
+    res = await db.execute(query)
+    return res.scalar_one_or_none()
+
+# 更新用户信息
+async def update_user_info(db: AsyncSession, username: str, user_data: UserUpdateRequest):
+    query = update(User).where(User.username == username).values(**user_data.model_dump(
+        exclude_none=True,
+        exclude_unset=True
+    ))
+    res = await db.execute(query)
+    await db.commit()
+
+    # 检查是否更新了
+    if res.rowcount == 0:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    # 更新后的用户信息
+    update_user = await get_user_by_username(db, username)
+    return update_user
+
+# 修改密码
+async def change_password(db: AsyncSession, user: User, old_password: str, new_password: str):
+    if not security.verify_password(old_password, user.password):
+        return False
+
+    pwd = security.get_hashed_password(new_password)
+    user.password = pwd
+    db.add(user)
+    await db.commit()
+    await db.refresh(user)
+    return True
--- news_backend/routers/users.py
+++ news_backend/routers/users.py
@@ -1,15 +1,17 @@
 from fastapi import APIRouter, Depends, HTTPException
-from pydantic import BaseModel
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 
 from config.db_conf import get_db
-from schemas.users import UserRequest, UserAuthResponse, UserInfoResponse
+from schemas.users import UserRequest, UserAuthResponse, UserInfoResponse, UserUpdateRequest, UserChangePasswordRequest
 from crud import users
 from utils.response import success_response
+from utils.auth import get_current_user
+from models.users import User
 
 router = APIRouter(prefix="/api/user", tags=["user"])
 
+# 注册
 @router.post("/register")
 async def register(user_data: UserRequest, db: AsyncSession = Depends(get_db),):
   existing_user = await users.get_user_by_username(db, user_data.username)
@@ -30,3 +32,33 @@ async def register(user_data: UserRequest, db: AsyncSession = Depends(get_db),):
   # }
   res_data = UserAuthResponse(token=token, user_info=UserInfoResponse.model_validate(user))
   return success_response(message="注册成功!", data=res_data)
+
+# 登录
+@router.post("/login")
+async def user_login(user_data: UserRequest, db: AsyncSession = Depends(get_db)):
+  user = await users.verify_user(db, user_data)
+  if not user:
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="用户名或密码错误")
+
+  token = await users.create_token(db, user.id)
+  res_data = UserAuthResponse(token=token, user_info=UserInfoResponse.model_validate(user))
+  return success_response(message="登录成功!", data=res_data)
+
+# 获取用户信息
+@router.get("/info")
+async def get_user_info(user: User = Depends(get_current_user)):
+  return success_response(message="获取用户信息成功啦", data=UserInfoResponse.model_validate(user))
+
+# 更新用户信息
+@router.put("/update")
+async def update_user_info(user_data: UserUpdateRequest, user: User = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
+  update_user_info = await users.update_user_info(db, user.username,user_data)
+  return success_response(message="修改用户信息成功啦",data=UserInfoResponse.model_validate(update_user_info))
+
+# 更新密码
+@router.put("/password")
+async def update_password(pwd_data: UserChangePasswordRequest, user: User = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
+  result = await users.change_password(db, user, pwd_data.old_password, pwd_data.new_password)
+  if not result:
+    raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="旧密码错误")
+  return success_response(message="修改密码成功")
